31a1705ad4ab3e42fb257c7e29c4d7e04117dfe5
Juliano Sill <sill.juliano@gmail.com>
2024-06-29 22:19:05 -0300

    refactor(pagination): paginate straight on instead of props

--- __tests__/components/pagination.test.tsx
+++ __tests__/components/pagination.test.tsx
@@ -4,42 +4,26 @@ import { fireEvent, render } from '@testing-library/react'
 
 import { Pagination } from '@/components/pagination'
 
-const onPageChangeCalback = jest.fn()
+jest.mock('next/navigation', () => ({
+  usePathname: () => '',
+  useSearchParams: () => '',
+  useRouter: () => {
+    return { push: routerPush }
+  },
+}))
+
+const routerPush = jest.fn()
 
 describe('Pagination component', () => {
-  it('should not render with more pages than exist', () => {
-    const wrapper = render(
-      <Pagination
-        currentPage={2}
-        totalItems={5}
-        perPage={10}
-        onPageChange={() => {}}
-      />,
-    )
-
-    const errorMessage = wrapper.getByText(
-      'Erro na renderização do componente.',
-    )
-    expect(errorMessage).toBeInTheDocument()
-  })
-
   it('should show 5 of 5 items on first page', () => {
-    const wrapper = render(
-      <Pagination currentPage={1} totalItems={5} onPageChange={() => {}} />,
-    )
-
+    const wrapper = render(<Pagination currentPage={1} totalItems={5} />)
     const listItems = wrapper.getByText('Exibindo 5 de 5 itens')
     expect(listItems).toBeInTheDocument()
   })
 
   it('should show last items on last page', () => {
     const wrapper = render(
-      <Pagination
-        currentPage={3}
-        totalItems={28}
-        perPage={10}
-        onPageChange={() => {}}
-      />,
+      <Pagination currentPage={3} totalItems={28} perPage={10} />,
     )
 
     const listItems = wrapper.getByText('Exibindo 8 de 28 itens')
@@ -49,14 +33,19 @@ describe('Pagination component', () => {
     expect(pageSelector).toHaveTextContent('3')
   })
 
+  it('should go to last page if render with current page higher than total pages', () => {
+    const wrapper = render(
+      <Pagination currentPage={5} totalItems={25} perPage={10} />,
+    )
+    const pagination = wrapper.getByRole('pagination')
+
+    expect(pagination).toBeInTheDocument()
+    expect(routerPush).toHaveBeenCalledWith('?page=3')
+  })
+
   it('should navigation buttons be enabled', () => {
     const wrapper = render(
-      <Pagination
-        currentPage={2}
-        totalItems={28}
-        perPage={10}
-        onPageChange={() => {}}
-      />,
+      <Pagination currentPage={2} totalItems={28} perPage={10} />,
     )
 
     const firstButton = wrapper.getByRole('button', {
@@ -74,10 +63,45 @@ describe('Pagination component', () => {
     expect(lastButton).toBeEnabled()
   })
 
-  it('should first and previous buttons be disabled on first page', () => {
+  it('should be able to navigate to the next page', () => {
+    const wrapper = render(<Pagination currentPage={2} totalItems={28} />)
+
+    const nextButton = wrapper.getByRole('button', { name: /próxima página/i })
+    fireEvent.click(nextButton)
+    expect(routerPush).toHaveBeenCalledWith('?page=3')
+  })
+
+  it('should be able to navigate to the previous page', () => {
+    const wrapper = render(<Pagination currentPage={3} totalItems={32} />)
+    const previoustButton = wrapper.getByRole('button', {
+      name: /página anterior/i,
+    })
+    fireEvent.click(previoustButton)
+    expect(routerPush).toHaveBeenCalledWith('?page=2')
+  })
+
+  it('should be able to navigate to the last page', () => {
     const wrapper = render(
-      <Pagination currentPage={1} totalItems={28} onPageChange={() => {}} />,
+      <Pagination currentPage={1} totalItems={52} perPage={10} />,
     )
+
+    const lastButton = wrapper.getByRole('button', { name: /última página/i })
+    fireEvent.click(lastButton)
+    expect(routerPush).toHaveBeenCalledWith('?page=6')
+  })
+
+  it('should be able to navigate to the first page', () => {
+    const wrapper = render(<Pagination currentPage={4} totalItems={58} />)
+
+    const firstButton = wrapper.getByRole('button', {
+      name: /primeira página/i,
+    })
+    fireEvent.click(firstButton)
+    expect(routerPush).toHaveBeenCalledWith('?page=1')
+  })
+
+  it('should first and previous buttons be disabled on first page', () => {
+    const wrapper = render(<Pagination currentPage={1} totalItems={28} />)
 
     const firstButton = wrapper.getByRole('button', {
       name: /primeira página/i,
@@ -85,86 +109,18 @@ describe('Pagination component', () => {
     const previousButton = wrapper.getByRole('button', {
       name: /página anterior/i,
     })
-
     expect(firstButton).toBeDisabled()
     expect(previousButton).toBeDisabled()
   })
 
   it('should next and last buttons be disabled on last page', () => {
     const wrapper = render(
-      <Pagination
-        currentPage={3}
-        totalItems={28}
-        perPage={10}
-        onPageChange={() => {}}
-      />,
+      <Pagination currentPage={3} totalItems={28} perPage={10} />,
     )
 
     const nextButton = wrapper.getByRole('button', { name: /próxima página/i })
     const lastButton = wrapper.getByRole('button', { name: /última página/i })
-
     expect(nextButton).toBeDisabled()
     expect(lastButton).toBeDisabled()
-  })
-
-  it('should be able to navigate to the next page', () => {
-    const wrapper = render(
-      <Pagination
-        currentPage={1}
-        totalItems={28}
-        onPageChange={onPageChangeCalback}
-      />,
-    )
-
-    const nextButton = wrapper.getByRole('button', { name: /próxima página/i })
-    fireEvent.click(nextButton)
-    expect(onPageChangeCalback).toHaveBeenCalledWith(2)
-  })
-
-  it('should be able to navigate to the last page', () => {
-    const wrapper = render(
-      <Pagination
-        currentPage={1}
-        totalItems={52}
-        perPage={10}
-        onPageChange={onPageChangeCalback}
-      />,
-    )
-
-    const lastButton = wrapper.getByRole('button', { name: /última página/i })
-    fireEvent.click(lastButton)
-    expect(onPageChangeCalback).toHaveBeenCalledWith(6)
-  })
-
-  it('should be able to navigate to the previous page', () => {
-    const wrapper = render(
-      <Pagination
-        currentPage={2}
-        totalItems={28}
-        onPageChange={onPageChangeCalback}
-      />,
-    )
-
-    const previoustButton = wrapper.getByRole('button', {
-      name: /página anterior/i,
-    })
-    fireEvent.click(previoustButton)
-    expect(onPageChangeCalback).toHaveBeenCalledWith(1)
-  })
-
-  it('should be able to navigate to the first page', () => {
-    const wrapper = render(
-      <Pagination
-        currentPage={4}
-        totalItems={58}
-        onPageChange={onPageChangeCalback}
-      />,
-    )
-
-    const firstButton = wrapper.getByRole('button', {
-      name: /primeira página/i,
-    })
-    fireEvent.click(firstButton)
-    expect(onPageChangeCalback).toHaveBeenCalledWith(1)
   })
 })
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,14 +1,14 @@
-'use client'
-
 import {
+  ChevronFirst,
+  ChevronLast,
   ChevronLeft,
   ChevronRight,
-  ChevronsLeft,
-  ChevronsRight,
 } from 'lucide-react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import type { ComponentProps } from 'react'
 
 import { cn } from '@/lib/utils'
+import { updateParams } from '@/utils/update-params'
 
 import { Button } from './ui/button'
 import { Select } from './ui/select'
@@ -18,26 +18,32 @@ export interface PaginationProps extends ComponentProps<'div'> {
   currentPage: number
   totalItems: number
   perPage?: number
-  onPageChange: (pageIndex: number) => Promise<void> | void
 }
 
 export function Pagination({
   currentPage,
   totalItems,
   perPage = 10,
-  onPageChange,
   className,
   ...props
 }: PaginationProps) {
-  const totalPages = Math.ceil(totalItems / perPage)
-  const lastPageItems = totalItems - (totalPages - 1) * perPage
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
-  if (currentPage > totalPages) {
-    return <p>Erro na renderização do componente.</p>
+  if (totalItems <= 0) {
+    return null
+  }
+
+  const lastPage = Math.ceil(totalItems / perPage)
+  const lastPageItems = totalItems - (lastPage - 1) * perPage
+
+  if (currentPage > lastPage) {
+    handlePaginate(lastPage)
   }
 
   const isFirstPage = currentPage === 1
-  const isLastPage = currentPage === totalPages
+  const isLastPage = currentPage === lastPage
 
   const numberOfItems = () => {
     if (totalItems <= perPage) {
@@ -49,10 +55,20 @@ export function Pagination({
     return perPage
   }
 
+  function handlePaginate(page: number) {
+    const params = updateParams({
+      searchParams,
+      deleteParams: page === 1 ? ['page'] : undefined,
+      params: page > 1 ? [{ key: 'page', value: page.toString() }] : undefined,
+    })
+    router.push(`${pathname}?${params}`)
+  }
+
   return (
     <div
+      role="pagination"
       className={cn(
-        'flex flex-wrap items-center justify-between gap-6 pt-6 text-sm',
+        'flex flex-wrap items-center justify-between gap-6 text-sm',
         className,
       )}
       {...props}
@@ -62,90 +78,92 @@ export function Pagination({
         {totalItems === 1 ? 'item' : 'itens'}
       </div>
 
-      <div className="flex flex-wrap items-center gap-x-6 gap-y-4">
-        <div className="flex items-center gap-2">
-          <span>Página</span>
-          <Select.Root
-            value={String(currentPage)}
-            onValueChange={(value: string) => onPageChange(Number(value))}
-          >
-            <Select.Trigger className="h-9 min-w-16 gap-1.5">
-              <Select.Value placeholder="1" />
-            </Select.Trigger>
-            <Select.Content>
-              {Array.from({ length: totalPages }).map((_, i) => {
-                const pageNumber = String(i + 1)
-                return (
-                  <Select.Item key={pageNumber} value={pageNumber}>
-                    {pageNumber}
-                  </Select.Item>
-                )
-              })}
-            </Select.Content>
-          </Select.Root>
-        </div>
+      {lastPage > 1 && (
+        <div className="flex flex-wrap items-center gap-x-6 gap-y-4">
+          <div className="flex items-center gap-2">
+            <span>Página</span>
+            <Select.Root
+              value={String(currentPage)}
+              onValueChange={(value: string) => handlePaginate(Number(value))}
+            >
+              <Select.Trigger className="h-9 min-w-16 gap-1.5">
+                <Select.Value placeholder="1" />
+              </Select.Trigger>
+              <Select.Content>
+                {Array.from({ length: lastPage }).map((_, i) => {
+                  const pageNumber = String(i + 1)
+                  return (
+                    <Select.Item key={pageNumber} value={pageNumber}>
+                      {pageNumber}
+                    </Select.Item>
+                  )
+                })}
+              </Select.Content>
+            </Select.Root>
+          </div>
 
-        <div className="flex gap-2">
-          <Tooltip.Provider>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Button
-                  variant="outline"
-                  className="size-9 p-0"
-                  disabled={isFirstPage}
-                  onClick={() => onPageChange(1)}
-                >
-                  <ChevronsLeft className="size-4" />
-                  <span className="sr-only">Primeira página</span>
-                </Button>
-              </Tooltip.Trigger>
-              <Tooltip.Content>Primeira página</Tooltip.Content>
-            </Tooltip.Root>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Button
-                  variant="outline"
-                  className="size-9 p-0"
-                  disabled={isFirstPage}
-                  onClick={() => onPageChange(currentPage - 1)}
-                >
-                  <ChevronLeft className="size-4" />
-                  <span className="sr-only">Página anterior</span>
-                </Button>
-              </Tooltip.Trigger>
-              <Tooltip.Content>Página anterior</Tooltip.Content>
-            </Tooltip.Root>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Button
-                  variant="outline"
-                  className="size-9 p-0"
-                  disabled={isLastPage}
-                  onClick={() => onPageChange(currentPage + 1)}
-                >
-                  <ChevronRight className="size-4" />
-                  <span className="sr-only">Próxima página</span>
-                </Button>
-              </Tooltip.Trigger>
-              <Tooltip.Content>Próxima página</Tooltip.Content>
-            </Tooltip.Root>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Button
-                  variant="outline"
-                  className="size-9 p-0"
-                  disabled={isLastPage}
-                  onClick={() => onPageChange(totalPages)}
-                >
-                  <ChevronsRight className="size-4" />
-                  <span className="sr-only">Última página</span>
-                </Button>
-              </Tooltip.Trigger>
-              <Tooltip.Content>Última página</Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
+          <div className="flex gap-2">
+            <Tooltip.Provider>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <Button
+                    variant="outline"
+                    className="size-9 p-0"
+                    disabled={isFirstPage}
+                    onClick={() => handlePaginate(1)}
+                  >
+                    <ChevronFirst className="size-4" />
+                    <span className="sr-only">Primeira página</span>
+                  </Button>
+                </Tooltip.Trigger>
+                <Tooltip.Content>Primeira página</Tooltip.Content>
+              </Tooltip.Root>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <Button
+                    variant="outline"
+                    className="size-9 p-0"
+                    disabled={isFirstPage}
+                    onClick={() => handlePaginate(currentPage - 1)}
+                  >
+                    <ChevronLeft className="size-4" />
+                    <span className="sr-only">Página anterior</span>
+                  </Button>
+                </Tooltip.Trigger>
+                <Tooltip.Content>Página anterior</Tooltip.Content>
+              </Tooltip.Root>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <Button
+                    variant="outline"
+                    className="size-9 p-0"
+                    disabled={isLastPage}
+                    onClick={() => handlePaginate(currentPage + 1)}
+                  >
+                    <ChevronRight className="size-4" />
+                    <span className="sr-only">Próxima página</span>
+                  </Button>
+                </Tooltip.Trigger>
+                <Tooltip.Content>Próxima página</Tooltip.Content>
+              </Tooltip.Root>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <Button
+                    variant="outline"
+                    className="size-9 p-0"
+                    disabled={isLastPage}
+                    onClick={() => handlePaginate(lastPage)}
+                  >
+                    <ChevronLast className="size-4" />
+                    <span className="sr-only">Última página</span>
+                  </Button>
+                </Tooltip.Trigger>
+                <Tooltip.Content>Última página</Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
